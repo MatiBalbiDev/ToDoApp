dc3147634ea0166266ccf5bfc6f926bef25ab189
MatiBalbiDev <matiasbalbi96@gmail.com>
2023-06-20 16:51:05 -0300

    Changes in component styles

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,53 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Item } from 'src/app/models/item.model';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-
-
-  /**
-   * TODO:
-   * - Borrar tarea
-   * - Editar tarea
-   * - Marcar tarea como realizada
-   * - Persistir tarea
-   * - Agregar hover sobre la tarea
-   * - Agregar iconos para editar, borrar y confirmar o hacerlo haciendo clicks..
-   * - Mostrar la cantidad de tareas realizadas/pendientes
-   * - Subirlo al repo y deployarlo
-   * 
-   * 
-   */
-
-  /* @Input() */
-
   public valorIngresado: string = '';
 
-  public item : Item;
+  public item: Item;
 
-  public listaDeItems: Item[] = [];  
-  
+  public listaDeItems: Item[] = [];
+
   constructor() {
     this.item = {
       id: 0,
       description: '',
-      checked: false
+      checked: false,
     };
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  guardarValor() : void {
-    if(this.valorIngresado.length)
-    this.item = {
-      id: this.listaDeItems.length + 1,
-      description: this.valorIngresado,
-      checked: false
-    }
+  guardarValor(): void {
+    if (this.valorIngresado.length)
+      this.item = {
+        id: this.listaDeItems.length + 1,
+        description: this.valorIngresado,
+        checked: false,
+      };
     this.listaDeItems.push(this.item);
     this.limpiarValor();
   }
