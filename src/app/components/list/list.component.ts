2c38310664fd0757574675a0956e0eddda05c049
MatiBalbiDev <matiasbalbi96@gmail.com>
2023-06-19 23:17:19 -0300

    Styling app

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  @Input()
+  item: string = "";
+
+  public listaDeItems: string[] = [];  
+  
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  guardarValor(item: string) : void {
+    this.listaDeItems.push(item);
+    this.limpiarValor();
+  }
+
+  limpiarValor(): void {
+    this.item = '';
+  }
 }
